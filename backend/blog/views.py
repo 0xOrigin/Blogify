d1c4a60c1b74cb7919ba2cad8c148aab7820b2a1
0xOrigin <egyahmed.ezzat120@gmail.com>
2024-03-13 01:22:45 +0200

    Add Post viewset

---
+++ backend/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from core.views import BaseViewSet
+from blog.models import Post
+from blog.serializers import PostSerializer
+from blog.filters import PostFilter
+from blog.permissions import BlogPermissions
+
+
+class PostViewSet(BaseViewSet):
+    model = Post
+    queryset = model.objects.all()
+    serializer_class = PostSerializer
+    filterset_class = PostFilter
+    permission_classes = [BlogPermissions,]
+    pagination_class = None
+
+    def get_queryset(self):
+        return super().get_queryset().select_related('author')
