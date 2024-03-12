4aebfd0430d692b3b246344ba6d4aa47635a0db0
0xOrigin <egyahmed.ezzat120@gmail.com>
2024-03-13 01:21:43 +0200

    Add Post permissions

---
+++ backend/blog/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+from core.permissions import BasePermissions
+
+
+class BlogPermissions(BasePermissions):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author.id == request.user.id
