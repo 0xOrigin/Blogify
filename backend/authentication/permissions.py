13ebe04ea4a2497573a9bbe5cdb257508257d241
0xOrigin <egyahmed.ezzat120@gmail.com>
2024-03-13 15:32:22 +0200

    Fix issue in auth permissions

--- backend/authentication/permissions.py
+++ backend/authentication/permissions.py
@@ -7,7 +7,7 @@ class AuthenticationPermissions(DjangoModelPermissions):
     def has_permission(self, request, view):
         if view.action == 'list':
             return request.user.is_superuser
-        return super().has_permission(request, view)
+        return True
 
     def has_object_permission(self, request, view, obj):
         return obj.id == request.user.id or request.user.is_superuser
